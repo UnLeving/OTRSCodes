8c473c9b108fbdb6a9ac44ef8f7752affbb85976
Levandovskyi Artem <336382@gmail.com>
2018-12-17 21:44:36 +0200

    add - column - row - cell - all cells

--- otrsCodes/Scripts/My/Code.js
+++ otrsCodes/Scripts/My/Code.js
@@ -111,37 +111,59 @@ $('body').on('contextmenu', 'td', function () {
     //});
 }
 
+// select and add column 
 $('body').on('click', 'thead th', function () {
     var codes = [];
     var tds = [];
+    var flag = false;
+
     var tbl = this.closest('table');
-    
-    var n = parseInt(this.textContent,10) + 2;
+    var n = parseInt(this.textContent, 10) + 2;
     for (var i = 1; i < tbl.rows.length; ++i) {
         codes.push(tbl.rows[i].cells[n].textContent);
         tds.push(tbl.rows[i].cells[n]);
     }
-    SendCodesOnServer(codes, tds);
+    SendCodesOnServer(codes, tds, flag);
 });
 
-// add code
+// select and add cell or all cells byctrl pressed
 $('body').on('click', 'tbody td', function () {
+    var flag;
     var codes = [];
     var tds = [];
     if (cntrlIsPressed === true) {
+        flag = true;
         tds = $(this.closest('tbody')).children();
         for (var i = 0; i < tds.length; ++i) {
-            codes.push(tds[i].textContent);
+            for (var j = 2; j < 12; ++j) {
+                codes.push(tds[i].cells[j].textContent);
+            }
         }
-    } else {
-        codes.push(this.textContent);
-        tds.push(this);
+        SendCodesOnServer(codes, tds, flag);
     }
-    //document.getElementById("Logs").value = tds.length;
-    //SendCodesOnServer(codes, tds);
+    else {
+        flag = false;
+        tds.push(this);
+        codes.push(this.textContent);
+        SendCodesOnServer(codes, tds, flag);
+    }
 });
 
-function SendCodesOnServer(codes, tds) {
+// select and add row
+$('body').on('click', 'tbody th', function () {
+    var codes = [];
+    var tds = [];
+    var flag = false;
+    codes.push(this.textContent);
+    tds = $(this).parent().children('td');
+    for (var i = 0; i < tds.length; ++i) {
+        codes.push(tds[i].textContent);
+    }
+
+    SendCodesOnServer(codes, tds, flag);
+});
+
+function SendCodesOnServer(codes, tds, isTwoDemenArr) {
     $.ajax({
         url: "/Codes/CreateMulti",
         type: "POST",
@@ -152,13 +174,10 @@ function SendCodesOnServer(codes, tds) {
             Value: codes
         },
         success: function () {
-            if (tds.length > 0) {
-                for (var i = 0; i <= tds.length; ++i) {
-                    $(tds).css('background-color', document.getElementById("HexSaver").value);
-                }
-            }
-            else {
-                $(this).css('background-color', document.getElementById("HexSaver").value);
+            if (isTwoDemenArr) {
+                OnSuccessTwoDimenArray(tds);
+            } else {
+                OnSuccessArray(tds);
             }
             document.getElementById("Logs").value = "200 OK";
         },
@@ -166,4 +185,22 @@ function SendCodesOnServer(codes, tds) {
             document.getElementById("Logs").value = status.statusText;
         }
     });
+}
+
+function OnSuccessTwoDimenArray(tds) {
+    if (tds.length > 0) {
+        for (var i = 0; i < tds.length; ++i) {
+            for (var j = 2; j < 12; ++j) {
+                $(tds[i].cells[j]).css('background-color', document.getElementById("HexSaver").value);
+            }
+        }
+    }
+}
+
+function OnSuccessArray(tds) {
+    if (tds.length > 0) {
+        for (var i = 0; i <= tds.length; ++i) {
+            $(tds).css('background-color', document.getElementById("HexSaver").value);
+        }
+    }
 }
