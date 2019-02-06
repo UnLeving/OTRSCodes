5fab2b27ef39574dad0db914fd7356f3e19be764
Levandovskyi Artem <336382@gmail.com>
2019-02-07 01:11:38 +0200

    one code reduce

--- otrsCodes/Controllers/CodesController.cs
+++ otrsCodes/Controllers/CodesController.cs
@@ -9,32 +9,62 @@ namespace otrsCodes.Controllers
 {
     public class CodesController : Controller
     {
-        private Model db = new Model();
+        private Model _db = new Model();
 
         [HttpPost]
         public ActionResult CreateMulti([Bind(Include = "CountryId,NetworkId,Zone,Value")] Codes codes)
         {
             if (ModelState.IsValid)
             {
-                var counter = 0;
+                ushort _addedCodes = 0;
+
+                // TODO: преобразовать выражение для сравнения коллекций ??
                 foreach (var code in codes.Value)
                 {
-                    var cd = db.Codes.Where(c => c.Value == code).FirstOrDefault();
-                    if (cd == null)
+                    var codeInDb = _db.Codes.Where(c => c.Zone == codes.Zone && c.Value == code).FirstOrDefault();
+                    if (codeInDb == null)
                     {
-                        db.Codes.Add(new Code() { CountryId = codes.CountryId, NetworkId = codes.NetworkId, Zone = codes.Zone, Value = code });
-                        ++counter;
+                        #region Reduce codes
+                        if (codes.Zone > 10)
+                        {
+                            if (codes.Value.Count() == 1)
+                            {
+                                var _inLineDBCodes = _db.Codes.Where(c => c.Zone == codes.Zone && c.Value.StartsWith(code.Remove(code.Length - 1)));
+                                if(_inLineDBCodes.Count() == 9)
+                                {
+                                    string _parentCode = codes.Zone % 10 + code.Remove(code.Length - 1);
+                                    _db.Codes.Add(new Code() { CountryId = codes.CountryId, NetworkId = codes.NetworkId, Zone = codes.Zone / 10, Value = _parentCode });
+                                    ++_addedCodes;
+                                    _db.Codes.RemoveRange(_inLineDBCodes);
+                                    break;
+
+                                    // TODO: triger table update
+                                }
+                            }
+                            else if (codes.Value.Count() == 10)
+                            {
+                                string _parentCode = codes.Zone % 10 + code.Remove(code.Length - 1);
+                                _db.Codes.Add(new Code() { CountryId = codes.CountryId, NetworkId = codes.NetworkId, Zone = codes.Zone / 10, Value = _parentCode });
+                                ++_addedCodes;
+                                break;
+                            }
+                        }
+                        #endregion
+                        _db.Codes.Add(new Code() { CountryId = codes.CountryId, NetworkId = codes.NetworkId, Zone = codes.Zone, Value = code });
+                        ++_addedCodes;
                     }
                     else
                     {
-                        if (cd.NetworkId == codes.NetworkId) continue;
-                        cd.NetworkId = codes.NetworkId;
-                        db.Entry(cd).State = EntityState.Modified;
-                        ++counter;
+                        if (codeInDb.NetworkId == codes.NetworkId) continue;
+                        codeInDb.NetworkId = codes.NetworkId;
+                        _db.Entry(codeInDb).State = EntityState.Modified;
+                        ++_addedCodes;
                     }
                 }
-                if (counter == 0) return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Nothing to change");
-                db.SaveChanges();
+                if (_addedCodes == 0)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Nothing to change");
+
+                _db.SaveChanges();
                 return new HttpStatusCodeResult(HttpStatusCode.OK);
             }
             return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Network not selected");
@@ -50,14 +80,14 @@ namespace otrsCodes.Controllers
             Code code;
             foreach (var id in ids)
             {
-                code = db.Codes.Find(id);
+                code = _db.Codes.Find(id);
                 if (code == null)
                 {
                     continue;
                 }
-                db.Codes.Remove(code);
+                _db.Codes.Remove(code);
             }
-            db.SaveChanges();
+            _db.SaveChanges();
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
@@ -65,7 +95,7 @@ namespace otrsCodes.Controllers
         {
             if (disposing)
             {
-                db.Dispose();
+                _db.Dispose();
             }
             base.Dispose(disposing);
         }
